f45a444fe26f5369270578c0e9df9b112ab16b41
kideveloper612 <developer61510@gmail.com>
2020-07-06 18:34:43 +0800

    initial

---
+++ alibaba.py
@@ -0,0 +1,18 @@
+import os
+import csv
+import time
+import requests
+from bs4 import BeautifulSoup
+
+
+def send_request(url, method):
+    headers = {
+        'user-agent':
+    }
+
+def main():
+
+
+if __name__ == '__main__':
+    category_url = 'https://www.alibaba.com/Products'
+    main()
